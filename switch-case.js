6e2dd34d19f7ba69f5b03021a5b599dce3bad85b
moshiur769 <moshiurrahaman769@gmail.com>
2023-09-15 23:32:33 +0600

    loop case

---
+++ switch-case.js
@@ -0,0 +1,31 @@
+const grade = 50;
+
+switch (true){
+    case grade >=90:
+        console.log('A+');
+        break;
+
+    case grade >=80:
+        console.log("A");
+        break;
+        
+    case grade >=70:
+        console.log("B+");
+        break;
+        
+    case grade >=60:
+        console.log("B");
+        break;
+        
+    case grade >=50:
+        console.log("c+");
+        break;
+        
+    case grade >=40:
+        console.log("c");
+        break;
+        
+    case grade >=30:
+        console.log("F");
+        break;    
+}
---
+++ while-break.js
@@ -0,0 +1,11 @@
+let i = 0;
+while ( i<6 ){
+    if (i === 3){
+        break;
+    }
+    console.log(i);
+    i += 1;
+}
+
+
+
